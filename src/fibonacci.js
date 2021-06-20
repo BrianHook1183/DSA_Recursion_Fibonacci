2e3788af6c879e647bb385ae9b767b0beafc5925
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-20 18:49:08 -0500

    initial commit | qualified prompt

---
+++ src/fibonacci.js
@@ -0,0 +1,9 @@
+//TODO Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. The 0th Fibonacci number is defined as 0 and the 1st Fibonacci number is defined as 1. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 0, 1, 1, 2, 3, 5, 8, 13... .
+
+/**
+ * Return the nth fibonacci number
+ * @param {integer} n
+ */
+function fibonacci(n) {}
+
+module.exports = fibonacci;

f04d66c26da24e09af1ebb409c3715f59c8d64f7
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-20 19:22:13 -0500

    submitted solution

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -4,6 +4,10 @@
  * Return the nth fibonacci number
  * @param {integer} n
  */
-function fibonacci(n) {}
+function fibonacci(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 2) + fibonacci(n - 1);
+}
 
 module.exports = fibonacci;
